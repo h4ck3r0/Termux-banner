a41c627c9eabd5e0ac162eb48b6079fba5f83605
H4Ck3R <46929618+h4ck3r0@users.noreply.github.com>
2021-06-08 14:46:58 +0530

    Update banner.sh

--- banner.sh
+++ banner.sh
@@ -3,9 +3,16 @@ sudo apt install figlet
 gem install lolcat
 clear
 
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
 banner() {
 
- figlet -f mono12 "kali-TH" | lolcat
+ 
+echo -e "${RED} ▀▀█▀▀ ░░ ▒█▀▀█ █▀▀█ █▀▀▄ █▀▀▄ █▀▀ █▀▀█ "
+echo -e "${RED} ░▒█░░ ▀▀ ▒█▀▀▄ █▄▄█ █░░█ █░░█ █▀▀ █▄▄▀ "
+echo -e "${RED} ░▒█░░ ░░ ▒█▄▄█ ▀░░▀ ▀░░▀ ▀░░▀ ▀▀▀ ▀░▀▀ ${ENDCOLOR} "
  echo""
  echo -e "\e[1;31m  [\e[32m√\e[31m] \e[1;91m by \e[1;36mRaj Aryan \e[93m/ \e[100;92 Youtube/H4Ck3R\e[0m"
 
@@ -22,22 +29,22 @@ wr () {
                                         sudo apt install wget
                                         sudo apt install curl
                                         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-                                        cd ~/kali-theme ; bash os.sh 
+                                        cd ~/termux-banner ; bash banner.sh
                                         
                                        }
                                        2line() {
                                                 sudo chsh -s zsh
                                                 cd ~/kali-theme/.object ; bash .1.sh
-                                                cd ~/kali-theme ; bash os.sh
+                                                cd ~/termux-banner ; bash banner.sh
                                                }
                                                3line() {
                                                        sudo chsh -s zsh 
                                                        cd ~/kali-theme/.object ; bash .2.sh                                                      
-                                                       cd ~/kali-theme ; bash os.sh
+                                                       cd ~/termux-banner ; bash banner.sh
                                                        
                                                          }
                                                           4line() {
-                                                                  rm -rf ~/kali-theme
+                                                                  rm -rf ~/termux-banner
                                                                   cd
                                                                   git clone https://github.com/h4ck3r0/kali-theme
                                                                   cd ~/termux-banner ; bash banner.sh
@@ -60,8 +67,8 @@ wr () {
 
   menu () {
                                   banner
-                                  printf "\n\033[1;91m[\033[0m1\033[1;91m]\033[1;92m Necessary Setup \n"
-                                  printf "\033[1;91m[\033[0m2\033[1;91m]\033[1;92m Zsh Theme\n"
+                                  printf "\n\033[1;91m[\033[0m1\033[1;91m]\033[1;92m Necessary \n"                                                                                                                                                               Setup \n"
+                                  printf "\033[1;91m[\033[0m2\033[1;91m]\033[1;92m Banners \n"
                                   printf "\033[1;91m[\033[0m3\033[1;91m]\033[1;92m Highlight & Suggestion\n"
                                   printf "\033[1;91m[\033[0m4\033[1;91m]\033[1;92m Update\n"
                                   printf "\033[1;91m[\033[0m5\033[1;91m]\033[1;92m Exit\n\n\n"
