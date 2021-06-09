3236886c5c25936577924387ae9c2ebccfe694b8
H4Ck3R <46929618+h4ck3r0@users.noreply.github.com>
2021-06-09 18:33:30 +0530

    Update banner.sh

--- banner.sh
+++ banner.sh
@@ -35,6 +35,7 @@ wr () {
                                                          apt install toilet figlet exa wget ruby 
                                                          rm -rf ~/.termux/colors.properties
                                                          rm -rf /data/data/com.termux/files/usr/etc/motd
+                                                         chsh -s zsh
                                                          cd ~/Termux-banner/.object ; cp -r .colors.properties ~/.termux/colors.properties
                                                          cd ~/Termux-banner/.object ; cp -r .termux.properties ~/.termux/termux.properties
                                                          cd ~/Termux-banner/.object ; bash .theme.sh
