af68b77a452d1fb71a4d6a55b1b65494c61d0d5f
H4Ck3R <46929618+h4ck3r0@users.noreply.github.com>
2021-06-09 18:57:29 +0530

    Update .theme.sh

--- .object/.theme.sh
+++ .object/.theme.sh
@@ -3,6 +3,7 @@ PUT(){ echo -en "\033[${1};${2}H";}
 clear
 git clone https://github.com/zsh-users/zsh-autosuggestions /data/data/com.termux/files/home/.oh-my-zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /data/data/com.termux/files/home/.oh-my-zsh/plugins/zsh-syntax-highlighting
+clear
 
 echo -e "  █ █▄░█ █▀ ▀█▀ ▄▀█ █░░ █░░ █▀▀ █▀█" | lolcat
 echo -e "  █ █░▀█ ▄█ ░█░ █▀█ █▄▄ █▄▄ ██▄ █▀▄" | lolcat
