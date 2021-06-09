b8dd71c4d8f45c874dff1d1590baaa41905ff084
H4Ck3R <46929618+h4ck3r0@users.noreply.github.com>
2021-06-09 19:31:16 +0530

    Update banner.sh

--- banner.sh
+++ banner.sh
@@ -31,7 +31,7 @@ wr () {
                                                          rm -rf ~/.oh-my-zsh
                                                          clear
                                                          wget -O $PREFIX/share/figlet/ASCII-Shadow.flf https://raw.githubusercontent.com/xero/figlet-fonts/master/ANSI%20Shadow.flf
-                                                         git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+                                                         
                                                          apt install toilet figlet exa wget ruby 
                                                          rm -rf ~/.termux/colors.properties
                                                          rm -rf /data/data/com.termux/files/usr/etc/motd
