59dc2abc55a1dffe9948897b1375ac743666d716
H4Ck3R <46929618+h4ck3r0@users.noreply.github.com>
2021-06-11 10:17:14 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,9 +17,19 @@ echo -e "${RED}     ░     ░  ░           ░           ${ENDCOLOR}   "
 echo  ""                                          
 echo -e "\e[1;34m[*] \e[32minstall packages....\e[0m";                                       
                                           
-                                          
-                                          
-                                          
+                                                         pkg install zsh -y
+                                                         pkg install git -y
+                                                         pkg install toilet figlet exa
+                                                         pkg install ruby  -y
+                                                         pkg install wget  -y
+                                                         gem install lolcat 
+                                                         pkg install curl -y
+                                                         pkg install zsh -y
+                                                         rm -rf ~/.oh-my-zsh
+                                                         clear 
+                                                         cd ~/Termux-banner ; bash banner.sh
+                                                         
+                                                                                                
                                           
                                           
                                           
