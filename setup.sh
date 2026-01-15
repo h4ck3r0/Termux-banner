c3711af0f7bdfc2f13324d5450a98ba0f6a4ab56
Raj Aryan <46929618+h4ck3r0@users.noreply.github.com>
2026-01-15 05:55:12 +0530

    Refactor setup.sh for improved readability
    Updated shebang and cleaned up package installation commands.

--- setup.sh
+++ setup.sh
@@ -1,35 +1,29 @@
-clear
+#!/data/data/com.termux/files/usr/bin/bash
 
+
+clear
 RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
 echo -e "${RED} ███████ ▓█████▄▄▄█████▓ █    ██  ██▓███  " 
-echo -e "${RED}▒██    ▒ ▓█   ▀▓  ██▒ ▓▒ ██  ▓██▒▓██░  ██▒" 
+echo -e "${RED}▒██   ▒ ▓█   ▀▓  ██▒ ▓▒ ██  ▓██▒▓██░  ██▒" 
 echo -e "${RED}░ ▓██▄   ▒███  ▒ ▓██░ ▒░▓██  ▒██░▓██░ ██▓▒" 
-echo -e "${RED}  ▒   ██▒▒▓█  ▄░ ▓██▓ ░ ▓▓█  ░██░▒██▄█▓▒ ▒" 
+echo -e "${RED}  ▒   ██▒▒▓█   ▄░ ▓██▓ ░ ▓▓█  ░██░▒██▄█▓▒ ▒" 
 echo -e "${RED}▒██████▒▒░▒████▒ ▒██▒ ░ ▒▒█████▓ ▒██▒ ░  ░" 
 echo -e "${RED}▒ ▒▓▒ ▒ ░░░ ▒░ ░ ▒ ░░   ░▒▓▒ ▒ ▒ ▒▓▒░ ░  ░" 
-echo -e "${RED}░ ░▒  ░ ░ ░ ░  ░   ░    ░░▒░ ░ ░ ░▒ ░     " 
-echo -e "${RED}░  ░  ░     ░    ░       ░░░ ░ ░ ░░       " 
-echo -e "${RED}     ░     ░  ░           ░           ${ENDCOLOR}   " 
-                                          
-echo  ""                                          
-echo -e "\e[1;34m[*] \e[32minstall packages....\e[0m";                                       
-                                          
-                                                         pkg install zsh -y
-                                                         pkg install git -y
-                                                         pkg install toilet figlet exa
-                                                         pkg install ruby  -y
-                                                         pkg install wget  -y
-                                                         gem install lolcat 
-                                                         pkg install curl -y
-                                                         pkg install zsh -y
-                                                         rm -rf ~/.oh-my-zsh
-                                                         clear 
-                                                         cd ~/Termux-banner ; bash banner.sh
-                                                         
-                                                                                                
-                                          
-                                          
-                                          
+echo -e "${RED}░ ░▒  ░ ░ ░ ░  ░    ░     ░░▒░ ░ ░ ░▒ ░      " 
+echo -e "${RED}░  ░  ░    ░    ░        ░░░ ░ ░ ░░        " 
+echo -e "${RED}     ░      ░  ░          ░              ${ENDCOLOR}    " 
+                                           
+echo  ""                                         
+echo -e "\e[1;34m[*] \e[32minstall packages....\e[0m";                            
+                                     
+# --- Install System Packages ---
+pkg update -y && pkg upgrade -y
+pkg install zsh -y
+pkg install git -y
+pkg install toilet figlet exa -y
+pkg install ruby -y
+pkg install wget curl -y
+gem install lolcat
